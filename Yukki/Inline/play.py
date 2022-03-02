333be67a1862a95a5e1113eba91af5bb37daa115
ZeaFeya <99448657+Kykoubot@users.noreply.github.com>
2022-03-03 01:36:49 +0800

    Update play.py

--- Yukki/Inline/play.py
+++ Yukki/Inline/play.py
@@ -16,7 +16,7 @@ def url_markup(videoid, duration, user_id, query, query_type):
                 callback_data=f"MusicStream {videoid}|{duration}|{user_id}",
             ),
             InlineKeyboardButton(
-                text="🎬",
+                text="📺",
                 callback_data=f"Choose {videoid}|{duration}|{user_id}",
             ),
             InlineKeyboardButton(
@@ -46,7 +46,7 @@ def url_markup2(videoid, duration, user_id):
                 callback_data=f"MusicStream {videoid}|{duration}|{user_id}",
             ),
             InlineKeyboardButton(
-                text="🎥 Putar Video",
+                text="📺 Putar Video",
                 callback_data=f"Choose {videoid}|{duration}|{user_id}",
             ),
         ],
@@ -77,21 +77,21 @@ def search_markup(
     buttons = [
         [
             InlineKeyboardButton(
-                text="1️⃣", callback_data=f"Yukki {ID1}|{duration1}|{user_id}"
+                text="1.", callback_data=f"Yukki {ID1}|{duration1}|{user_id}"
             ),
             InlineKeyboardButton(
-                text="2️⃣", callback_data=f"Yukki {ID2}|{duration2}|{user_id}"
+                text="2.", callback_data=f"Yukki {ID2}|{duration2}|{user_id}"
             ),
             InlineKeyboardButton(
-                text="3️⃣", callback_data=f"Yukki {ID3}|{duration3}|{user_id}"
+                text="3.", callback_data=f"Yukki {ID3}|{duration3}|{user_id}"
             ),
         ],
         [
             InlineKeyboardButton(
-                text="4️⃣", callback_data=f"Yukki {ID4}|{duration4}|{user_id}"
+                text="4.", callback_data=f"Yukki {ID4}|{duration4}|{user_id}"
             ),
             InlineKeyboardButton(
-                text="5️⃣", callback_data=f"Yukki {ID5}|{duration5}|{user_id}"
+                text="5.", callback_data=f"Yukki {ID5}|{duration5}|{user_id}"
             ),
         ],
         [
@@ -126,25 +126,25 @@ def search_markup2(
     buttons = [
         [
             InlineKeyboardButton(
-                text="6️⃣",
+                text="6.",
                 callback_data=f"Yukki {ID6}|{duration6}|{user_id}",
             ),
             InlineKeyboardButton(
-                text="7️⃣",
+                text="7.",
                 callback_data=f"Yukki {ID7}|{duration7}|{user_id}",
             ),
             InlineKeyboardButton(
-                text="8️⃣",
+                text="8.",
                 callback_data=f"Yukki {ID8}|{duration8}|{user_id}",
             ),
         ],
         [
             InlineKeyboardButton(
-                text="9️⃣",
+                text="9.",
                 callback_data=f"Yukki {ID9}|{duration9}|{user_id}",
             ),
             InlineKeyboardButton(
-                text="🔟",
+                text="10.",
                 callback_data=f"Yukki {ID10}|{duration10}|{user_id}",
             ),
         ],
